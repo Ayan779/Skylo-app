be2b390e60e9c8b88f99163db8d9fd50a285a4bd
Ayan779 <48341842+Ayan779@users.noreply.github.com>
2026-08-15 02:03:55 -0500

    Implement displayForecast function
    Add displayForecast function to show weather forecast.

--- index.js
+++ index.js
@@ -67,7 +67,36 @@ function handleSearch(event) {
   searchCity(searchInput.value);
 }
 
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+        <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-icon">🌤️</div>
+
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>15°</strong>
+            </div>
+            <div class="weather-forecast-temperature">10°</div>
+          </div>
+        </div>
+      `;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearch);
 
 searchCity("Mankato");
+displayForecast();
+
